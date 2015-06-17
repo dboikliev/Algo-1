f973b53a69fbab59f109a98f1b9b83d77745216e
dboikliev <deyanpb@hotmail.com>
2015-06-18 00:53:19 +0300

    Adding BirthdayRanges solution

--- week2/2-Birthday-Ranges/BirthdayRanges.java
+++ week2/2-Birthday-Ranges/BirthdayRanges.java
@@ -1,15 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class BirthdayRanges {
+    public static class Pair {
+        public int start;
+        public int end;
+    }
 
-  public static class Pair {
-
-    public int start;
-    public int end;
-  }
-
-  // Returns a vector with the number of people born in the specific ranges.
-  public List<Integer> birthdaysCount(List<Integer> birthdays, List<Pair> ranges) {
-    // ...
-  }
+    // Returns a vector with the number of people born in the specific ranges.
+    public List<Integer> birthdaysCount(List<Integer> birthdays, List<Pair> ranges) {
+        Collections.shuffle(birthdays);
+        BinarySearch search = new BinarySearch();
+        List<Integer> numbersOfBirthdays = new ArrayList<Integer>();
+        for (Pair range : ranges) {
+            int peopleInRange = 0;
+            for (int i = range.start; i <= range.end; i++) {
+                peopleInRange += search.find(birthdays, i) > - 1 ? 1 : 0;
+            }
+            numbersOfBirthdays.add(peopleInRange);
+        }
+        return numbersOfBirthdays;
+    }
 }
